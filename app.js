afd9632bf77c5e1f88a4dc4925d23ddb5cc833a3
manikyajabin <Manikya77787@gmail.com>
2025-03-19 11:36:09 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const{User}=require('./model/User');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {Product} = require('./model/Product');
 const {Cart} = require('./model/Cart');
@@ -15,8 +15,11 @@ app.use(cors());
 app.use(morgan('dev'))
 
 
+//vkuguIEGuqOzztxM
+//kkirankumarbanavi
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleProject')
+let MONGODB_URL = "mongodb+srv://kkirankumarbanavi:vkuguIEGuqOzztxM@cluster0.06v9z.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
